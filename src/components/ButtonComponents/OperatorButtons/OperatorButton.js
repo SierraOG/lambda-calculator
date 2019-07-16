f28e5e48d16eafff4e9e3109ff993d8abcc5d4a9
SierraOG <sierrao@utexas.edu>
2019-07-16 15:02:40 -0500

    finished steps 1-3

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = ({ope}) => {
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    <button> {ope.char} </button>
   );
 };
+
+export default OperatorButton
