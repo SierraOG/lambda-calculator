24b6bc2dedac4919696a8e82a3348cd3a08f02ef
SierraOG <sierrao@utexas.edu>
2019-07-16 19:01:17 -0500

    moved css into component folders

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
-
+import "./components/DisplayComponents/Display.css"
+import "./components/ButtonComponents/Buttons.css"
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
